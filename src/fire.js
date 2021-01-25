0f7fedc261ff8f2944a259dbc8c2a8f61cd9b210
Semal Sherathia <semal.sherathia@gmail.com>
2021-01-25 21:20:25 +0530

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "Your API KEY",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
+  apiKey: "AIzaSyC7llHDwFs5PJkO_p9i1zLqMNgy-Gxx90w",
+  authDomain: "reactauth-d5ad9.firebaseapp.com",
+  projectId: "reactauth-d5ad9",
+  storageBucket: "reactauth-d5ad9.appspot.com",
+  messagingSenderId: "374774375804",
+  appId: "1:374774375804:web:950022c8005877b79430f9",
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
